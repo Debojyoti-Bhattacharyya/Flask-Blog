998eb89c9fe8beada738a521f5ac6604c0d7fcb1
Debojyoti-Bhattacharyya <debojyotibhattacharyya@gmail.com>
2021-10-05 23:02:01 +0530

    commented site.db line

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '6348bc5ac63090a28b93b21561a53579'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)    # initialize database object
 
 
