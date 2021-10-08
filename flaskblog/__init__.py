7f0f065b1cbc0fee58c80da539dbb4d5c2117146
Debojyoti-Bhattacharyya <debojyotibhattacharyya@gmail.com>
2021-10-08 20:17:25 +0530

    uncommented site.db

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '6348bc5ac63090a28b93b21561a53579'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)    # initialize database object
 
 from flaskblog import routes
